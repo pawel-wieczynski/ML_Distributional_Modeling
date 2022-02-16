55d9eb372adf5e49eacc6d1c37794a9fddccd92b
TurboPavel <wieczynskipawel@gmail.com>
2022-02-16 18:03:37 +0100

    min-max normalization before split

--- data_partition.R
+++ data_partition.R
@@ -1,6 +1,15 @@
+dataset <- read.csv('mldm_dataset.csv')[-1]
+dataset[,1:612] <- scale(dataset[,1:612]
+                         ,center = sapply(dataset[,1:612], min)
+                         ,scale = sapply(dataset[,1:612], max) - sapply(dataset[,1:612], min))
+
 set.seed(2137)
-index <- caret::createDataPartition(dataset$V613, p = 0.75, list = FALSE)
+nrow <- dim(dataset)[1]
+index <- sample(1:nrow, 0.75 * nrow)
 train_set <- dataset[index,]
 test_set <- dataset[-index,]
 write.csv(train_set, 'train_data.csv', row.names = FALSE)
 write.csv(test_set, 'test_data.csv', row.names = FALSE)
+
+
+
